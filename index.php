132357c21001e9397711706a6710d346cac71e72
msalihov <msalihov@gmail.com>
2013-03-11 19:39:44 +0000

    Licensed!
    Totally safe now.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+
+Copyright 2013 Maxim Salikhov
+
+This file is part of PHP Bootstrap Forum.
+
+PHP Bootstrap Forum is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PHP Bootstrap Forum is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PHP Bootstrap Forum.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+?>
